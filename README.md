e4de8a4c59e3e3d565fc002f99ba35e3809a0c0a
postolenko <denis.postolenko@gmail.com>
2021-08-25 03:10:58 +0300

    MyCrane

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # http://postolenko.github.io/my_crane_24_08/vacansy.html
+# http://postolenko.github.io/my_crane_24_08/create_resume.html
--- main.js
+++ main.js
@@ -148,4 +148,13 @@ $(document).ready(function(){
       }
     });
 
+    // -----------
+
+    imgInp.onchange = evt => {
+      const [file] = imgInp.files
+      if (file) {
+        blah.src = URL.createObjectURL(file)
+      }
+    }
+
 });
